fd1535584c600295bb070b54588f5387bdeeeda0
arthur.alves <arthursalama@gmail.com>
2026-04-24 10:02:46 -0300

    last project commit

--- src/main/java/br/com/exercicio/tabelaFipe/TabelaFipeApplication.java
+++ src/main/java/br/com/exercicio/tabelaFipe/TabelaFipeApplication.java
@@ -4,13 +4,22 @@ import br.com.exercicio.tabelaFipe.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.Desktop;
+import java.net.URI;
 
 @SpringBootApplication
 public class TabelaFipeApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
+		Thread.sleep(2000);
+
+		Runtime.getRuntime().exec(
+				"cmd /c start chrome http://localhost:8080/fipe-search.html"
+		);
+
 		Principal principal = new Principal();
 		principal.exibeMenu();
+
 	}
 
 	public static void main(String[] args) {
